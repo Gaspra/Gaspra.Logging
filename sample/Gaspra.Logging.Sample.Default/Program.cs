8671781490c18164333edd14c84bb65af85ebfe5
Gaspra <35634732+Gaspra@users.noreply.github.com>
2019-10-07 15:27:16 +0100

    Add files via upload

---
+++ sample/Gaspra.Logging.Sample.Default/Program.cs
@@ -0,0 +1,26 @@
+﻿using Gaspra.Logging.Hosting;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using FifteenBelow.Logging.Sample.Default;
+
+namespace Gaspra.Logging.Sample.Default
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging
+                        .ClearProviders()
+                        .AddDefaultFluentd();
+                })
+                .UseStartup<Startup>();
+    }
+}
---
+++ sample/Gaspra.Logging.Sample.Default/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Gaspra.Logging.Sample.Default
+{
+    public class Startup
+    {
+        private readonly ILogger logger;
+
+        public Startup(ILogger<Startup> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            logger.LogInformation("Testing logging default setup: {name}", "Gaspra");
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            /*do nothing, configure services will run before and log*/
+        }
+    }
+}
